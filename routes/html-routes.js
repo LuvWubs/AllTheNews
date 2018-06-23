6830adcb54f48f08451687894346525ccef69ca5
LuvWubs <iydkwtmiwt@gmail.com>
2018-06-23 15:34:34 -0600

    scrape

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,12 +2,120 @@ var path = require('path');
 
 module.exports = function(app) {
 
-  app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../assets/public/main.html'));
+  // Database configuration
+  var databaseUrl = "scraper";
+  var collections = ["scrapedData"];
+
+  console.log(__dirname);
+  app.use('/public', express.static(path.join(__dirname, '/public')));
+
+  // Hook mongojs configuration to the db variable
+  var db = mongojs(databaseUrl, collections);
+  db.on("error", function(error) {
+    console.log("Database Error:", error);
   });
 
-  app.get('/articles/saved', function(req, res) {
-    res.sendFile(path.join(__dirname, '../assets/public/articles.html'))
+  app.get('/all', function(req, res) {
+    console.log('getting all articles');
+
+  })
+
+      app.get("/articles", function(req, res) {
+        console.log('scrape requested')
+        // // Make a request for the news section of `ycombinator`
+        request("https://www.bbc.com/news", function(error, response, body) {
+        //   console.log('req obj in scrape route: ');
+        //   // Load the html body from request into cheerio
+        // console.log(error)
+        // console.log(response)
+        // console.log(body)
+          // var $ = cheerio.load(html);
+
+          // console.log(cheerio);
+        //   // For each element with a "title" class
+        //   $(".headlines").each(function(i, element) {
+        //     // Save the text and href of each link enclosed in the current element
+        //     var title = $(element).children("a").text();
+        //     console.log('title of article: ', title);
+        //     var link = $(element).children("a").attr("href");
+        //     console.log('link of article: ', link);
+        //     console.log('is this werking???');
+        //     // If this found element had both a title and a link
+        //     if (title && link) {
+        //       // Insert the data in the scrapedData db
+        //       db.scrapedData.insert({
+        //         title: title,
+        //         link: link
+        //       },
+        //       function(err, inserted) {
+        //         if (err) {
+        //           // Log the error if one is encountered during the query
+        //           console.log(err);
+        //         }
+        //         else {
+        //           // Otherwise, log the inserted data
+        //           console.log(inserted);
+        //         }
+        //       });
+        //     }
+        //   });
+        });
+
+        // Send a "Scrape Complete" message
+        res.json(res);
+      });
+
+
+
+  // Main route (simple Hello World Message)
+  // app.get("/", function(req, res) {
+  //   res.send("Hello world");
+  // });
+
+  // TODO: make two more routes
+  app.get("/all", function(req, res) {
+    // Find all results from the scrapedData collection in the db
+    db.scrapedData.find({}, function(error, found) {
+      // Throw any errors to the console
+      if (error) {
+        console.log(error);
+      }
+      // If there are no errors, send the data to the browser as json
+      else {
+        res.json(found);
+      }
+    });
   });
+
+  // Route 1
+  // =======
+  // This route will retrieve all of the data
+  // from the scrapedData collection as a json (this will be populated
+  // by the data you scrape using the next route)
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/views/main.html'));
+  });
+
+  app.use(express.static('public'));
+
+  // Route 2
+  // =======
+  // When you visit this route, the server will
+  // scrape data from the site of your choice, and save it to
+  // MongoDB.
+  // TIP: Think back to how you pushed website data
+  // into an empty array in the last class. How do you
+  // push it into a MongoDB collection instead?
+
+  app.get('/articles/saved', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/public/articles.html'))
+  });
+
+  app.get('/articles/:query', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/public/articles.html'))
+  });
+
+  /* -/-/-/-/-/-/-/-/-/-/-/-/- */
+
 
 };
--- server.js
+++ server.js
@@ -1,38 +1,146 @@
-var cheerio = require("cheerio");
+// Using the tools and techniques you learned so far,
+// you will scrape a website of your choice, then place the data
+// in a MongoDB database. Be sure to make the database and collection
+// before running this exercise.
+
+// Consult the assignment files from earlier in class
+// if you need a refresher on Cheerio.
+
+// Dependencies
+var express = require("express");
+// var mongojs = require("mongojs");
+// var logger = require("morgan");
+// Require request and cheerio. This makes the scraping possible
 var request = require("request");
+var cheerio = require("cheerio");
+var axios = require("axios");
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
 
-// First, tell the console what server3.js is doing
-console.log("\n******************************************\n" +
-            "Look at the image of every award winner in \n" +
-            "one of the pages of `awwwards.com`. Then,\n" +
-            "grab the image's source URL." +
-            "\n******************************************\n");
+// Require all models
+// var db = require("./models");
+var PORT = 3080;
 
-// Make request to grab the HTML from `awwards's` clean website section
-request("http://www.awwwards.com/websites/clean/", function(error, response, html) {
+// Initialize Express
+var app = express();
 
-  // Load the HTML into cheerio
-  var $ = cheerio.load(html);
+// Use morgan logger for logging requests
+// app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+// Connect to the Mongo DB
 
-  // Make an empty array for saving our scraped info
-  var results = [];
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-  // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
-  $("figure.rollover").each(function(i, element) {
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
-    /* Cheerio's find method will "find" the first matching child element in a parent.
-     *    We start at the current element, then "find" its first child a-tag.
-     *    Then, we "find" the lone child img-tag in that a-tag.
-     *    Then, .attr grabs the imgs srcset value.
-     *    The srcset value is used instead of src in this case because of how they're displaying the images
-     *    Visit the website and inspect the DOM if there's any confusion
-    */
-    var imgLink = $(element).find("a").find("img").attr("data-srcset").split(",")[0].split(" ")[0];
 
-    // Push the image's URL (saved to the imgLink var) into the results array
-    results.push({ link: imgLink });
+// // Database configuration
+// var databaseUrl = "allTheNews";
+// var collections = ["Articles"];
+
+console.log(__dirname);
+app.use('/public', express.static(path.join(__dirname, '/public')));
+// app.use("models");
+
+// Hook mongojs configuration to the db variable
+// var db = mongojs(databaseUrl, collections);
+// db.on("error", function(error) {
+//   console.log("Database Error:", error);
+// });
+// Require all models
+var db = require("./models");
+
+
+app.get("/articles", function(req, res) {
+  console.log('scrape requested from server')
+  // // Make a request for the news section of `ycombinator`
+  request("https://futurism.com/robotsmachines/", function(error, response, body) {
+
+      var $ = cheerio.load(response.body);
+      // console.log('response.body: ', response.body);
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("h3 a").each(function(i, element) {
+
+        // Save an empty result object
+        var result = {};
+
+        // Add the text and href of every link, and save them as properties of the result object
+        result.number = i + 1;
+        result.title = element.attribs.title;
+        result.link = element.attribs.href;
+        console.log('result obj: ', result);
+
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+
+            // View the added result in the console
+            // console.log('these are the scraped articles: ', dbArticle);
+            console.log('these are the results: ', result);
+            $('#articles').append(result);
+          })
+          .catch(function(err) {
+            // If an error occurred, send it to the client
+            return res.json(err);
+          });
+      });
+
+    // Send a "Scrape Complete" message
+    res.send('scrape completed');
   });
+});
 
-  // After looping through each element found, log the results to the console
-  console.log(results);
+// TODO: make two more routes
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
+});
+
+// Route 1
+// =======
+// This route will retrieve all of the data
+// from the scrapedData collection as a json (this will be populated
+// by the data you scrape using the next route)
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/views/main.html'));
+});
+
+app.use(express.static('public'));
+
+// Route 2
+// =======
+// When you visit this route, the server will
+// scrape data from the site of your choice, and save it to
+// MongoDB.
+// TIP: Think back to how you pushed website data
+// into an empty array in the last class. How do you
+// push it into a MongoDB collection instead?
+
+app.get('/articles/saved', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/public/articles.html'))
+});
+//
+// app.get('/articles/:query', function(req, res) {
+//   res.sendFile(path.join(__dirname, '../public/public/articles.html'))
+// });
+
+/* -/-/-/-/-/-/-/-/-/-/-/-/- */
+
+// Listen on port 3000
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
